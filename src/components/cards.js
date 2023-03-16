f51935cb38117b7c24bfe0c7ce45d7747f2da79d
praveen747 <pravp747@gmail.com>
2023-03-16 17:07:19 -0500

    Cards

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,44 +1,44 @@
-import React, { useState } from "react";
-import { Card, Button } from "antd";
+import React, { useState } from 'react';
+import { Card, Button } from 'antd';
 
 const { Meta } = Card;
 
 const mockData = [
   {
     id: 1,
-    title: "Card 1",
-    description: "This is a card description",
-    imageUrl: "https://via.placeholder.com/300x200",
+    title: 'Card 1',
+    description: 'This is a card description',
+    imageUrl: 'https://via.placeholder.com/300x200',
   },
   {
     id: 2,
-    title: "Card 2",
-    description: "This is a card description",
-    imageUrl: "https://via.placeholder.com/300x200",
+    title: 'Card 2',
+    description: 'This is a card description',
+    imageUrl: 'https://via.placeholder.com/300x200',
   },
   {
     id: 3,
-    title: "Card 3",
-    description: "This is a card description",
-    imageUrl: "https://via.placeholder.com/300x200",
+    title: 'Card 3',
+    description: 'This is a card description',
+    imageUrl: 'https://via.placeholder.com/300x200',
   },
   {
     id: 4,
-    title: "Card 4",
-    description: "This is a card description",
-    imageUrl: "https://via.placeholder.com/300x200",
+    title: 'Card 4',
+    description: 'This is a card description',
+    imageUrl: 'https://via.placeholder.com/300x200',
   },
   {
     id: 5,
-    title: "Card 5",
-    description: "This is a card description",
-    imageUrl: "https://via.placeholder.com/300x200",
+    title: 'Card 5',
+    description: 'This is a card description',
+    imageUrl: 'https://via.placeholder.com/300x200',
   },
   {
     id: 6,
-    title: "Card 6",
-    description: "This is a card description",
-    imageUrl: "https://via.placeholder.com/300x200",
+    title: 'Card 6',
+    description: 'This is a card description',
+    imageUrl: 'https://via.placeholder.com/300x200',
   },
 ];
 
@@ -59,33 +59,32 @@ const MultiSelectCard = () => {
 
   return (
     <>
-      <div style={{ marginBottom: "10px" }}>
+      <div style={{ marginBottom: '10px' }}>
         <Button onClick={handleResetClick}>Reset</Button>
       </div>
-      <div style={{ display: "flex", justifyContent: "center" }}>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
         {mockData.map((card) => (
           <div
             key={card.id}
             style={{
-              width: "300px",
-              margin: "10px",
-              backgroundColor: selectedCards.includes(card.id)
-                ? "lightblue"
-                : "white",
-              cursor: "pointer",
+              width: '300px',
+              margin: '10px',
+              border: '1px solid #d9d9d9',
+              borderRadius: '2px',
+              backgroundColor: selectedCards.includes(card.id) ? 'lightblue' : 'white',
+              cursor: 'pointer',
             }}
             onClick={() => handleCardClick(card.id)}
           >
             <Card
-              hoverable
-              cover={<img alt={card.title} src={card.imageUrl} />}
-              style={{
-                height: "100%",
-                backgroundColor: "transparent",
-                border: "none",
-              }}
+              style={{ height: '100%', backgroundColor: 'transparent', border: 'none' }}
             >
-              <Meta title={card.title} description={card.description} />
+              <div style={{ height: '80px', padding: '8px' }}>
+                <Meta title={card.title} />
+              </div>
+              <div style={{ height: '120px', padding: '8px', borderTop: '1px solid #d9d9d9' }}>
+                <Meta description={card.description} />
+              </div>
             </Card>
           </div>
         ))}
